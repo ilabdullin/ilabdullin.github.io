be22db9b382d74c4363aea758576cb408152b33f
ilabdullin <ilnurabdullin578@gmail.com>
2020-09-17 15:08:26 +0500

    initial commit

---
+++ mogo/assets/js/app.js
@@ -0,0 +1,84 @@
+$(function() {
+
+	let header = $("#header"),
+		introH = $("#intro").innerHeight(),
+		scrollOffset = $(window).scrollTop();
+
+
+
+	/* Fixed Header */
+	checkScroll(scrollOffset);
+
+	$(window).on("scroll", function() {
+		scrollOffset = $(this).scrollTop();
+
+		checkScroll(scrollOffset);
+
+	});
+
+
+
+	function checkScroll(scrollOffset) {
+		if( scrollOffset > introH ) {
+			header.addClass("fixed");
+		}	else {
+			header.removeClass("fixed");
+		}
+	}
+
+
+
+	/* Smooth scroll */
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+
+		let $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset = $(blockId).offset().top;
+
+			$("#nav a").removeClass("active");
+			$this.addClass("active");
+
+		$("html, body").animate({
+			scrollTop: blockOffset + 5
+		}, 500);
+
+	});
+
+
+
+	/* Menu nav toggle */
+	$("#nav__toggle").on("click", function(event) {
+		event.preventDefault();
+
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+	});
+
+
+
+	/* Collapse */
+	$("[data-collapse]").on("click", function(event) {
+		event.preventDefault();
+
+		let $this = $(this),
+			blockId = $this.data('collapse');
+
+			$this.toggleClass("active");
+			$(blockId).slideToggle();
+	});
+
+
+
+	/* Slider */
+	$("[data-slider]").slick ({
+		infinite: true,
+		fade: true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+
+
+
+
+});
