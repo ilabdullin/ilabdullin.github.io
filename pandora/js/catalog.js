bc6ab5f41da87160b1ff48b0cf3d44b3a5895e5d
Ильнур <ilnurabdullin578@gmail.com>
2020-11-03 11:08:26 +0500

    initial commit

---
+++ pandora/js/catalog.js
@@ -0,0 +1,17 @@
+$(function() {
+
+ /* Reviews: https://kenwheeler.github.io/slick/ */
+    let slider = $("#catalogSlider");
+
+    slider.slick({
+        autoplay: false,
+        autoplaySpeed: 5000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        infinite: true
+    });
+
+
+
+});
